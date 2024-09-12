fee18b544f0276f67ed6fd6c95074253685463be
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-12 15:25:18 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ password = 'teste'
 senha = input("Informe a senha: ")
 if senha != password:
     while tentativa < 3 and senha != password:
-        print("Senha Incorreta tente Novamente!!!")
+        print("Senha Incorreta\ntente Novamente!!!")
         tentativa += 1
         senha = input("Informe a senha: ")
         if senha == password:
-            print("Senha Correta!!!\nAcesso Autorizado")
+            print("Senha Correta.\nAcesso Autorizado")
         elif tentativa == 3:
             print("Senha Incorreta.\nNúmero Máxima de tentativas atingidas.\nEssa conta será bloqueada!!!")
 else:
-    print("Senha correta!!!\nAcesso Autorizado")
+    print("Senha correta.\nAcesso Autorizado")
