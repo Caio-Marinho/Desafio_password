999a1a55b3afbdac702f652999d0a04878c86441
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-13 15:45:53 -0300

    versão somente com if's

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if senha != password:
             senha = input(f"{4 - tentativa} de 3 Tentativas - Informe a senha: ")
         if senha == password:
             print("Senha Correta.\nAcesso Autorizado")
-        elif tentativa > 3:
+        if tentativa > 3:
             print(f"Senha Incorreta.\n{4 - tentativa} de 3 Tentativas\n"
                   f"Número Máxima de tentativas atingidas.\nEssa conta será bloqueada!!!")
 else:
