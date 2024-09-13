c0755a6ee2f62d1c1890ba9a5c709eaa13637d7b
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-13 16:22:51 -0300

    versão somente com um if e um if/else

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ if senha != password:
         if tentativa <= 3:
             print("Senha Incorreta\ntente Novamente!!!")
             senha = input(f"{4 - tentativa} de 3 Tentativas - Informe a senha: ")
-        if senha == password:
-            print("Senha Correta.\nAcesso Autorizado")
-        if tentativa > 3:
+        else:
             print(f"Senha Incorreta.\n{4 - tentativa} de 3 Tentativas\n"
                   f"Número Máxima de tentativas atingidas.\nEssa conta será bloqueada!!!")
+        if senha == password:
+            print("Senha Correta.\nAcesso Autorizado")
+
 else:
     print("Senha correta.\nAcesso Autorizado")
